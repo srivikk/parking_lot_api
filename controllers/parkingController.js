97aeabcc5d09b6ead5d8cfd7208c37b614150d3a
Vikrant <srivasvik@gmail.com>
2020-07-22 15:50:27 +0530

    all APIs created and handled error

--- controllers/parkingController.js
+++ controllers/parkingController.js
@@ -2,11 +2,15 @@ const { callDB } = require('../models/db')
 
 exports.getLot = async (req, res) => {
     const lotList = await callDB('SELECT name FROM parking_lot')
-    lotListArray = []
-    lotList.forEach(element => {
-        lotListArray.push(element.name)
-    });
-    res.send(lotListArray)
+    if (lotListArray.length > 0) {
+        lotListArray = []
+        lotList.forEach(element => {
+            lotListArray.push(element.name)
+        });
+        res.send(lotListArray)
+    }
+    else
+        res.send({ "response": "No Parking lot available!" })
 };
 
 exports.getLotName = async (req, res) => {
@@ -14,23 +18,34 @@ exports.getLotName = async (req, res) => {
     const lotname = req.params.lot_name
     const lotDetails = await callDB('SELECT * FROM parking_lot where name="' + lotname + '"')
     const bayList = await callDB('SELECT * FROM bays where parkinglot_name="' + lotname + '"')
-    lotDetails[0].bays = []
-    bayList.forEach(element => {
-        lotDetails[0].bays.push(element.bay_name)
-    });
-
-    res.send(lotDetails)
+    if (lotDetails.length > 0) {
+        lotDetails[0].bays = []
+        bayList.forEach(element => {
+            lotDetails[0].bays.push(element.bay_name)
+        });
+        res.send(lotDetails)
+    }
+    else
+        res.send({ "response": "No such Parking lot name is available!" })
 };
 
 exports.getBayName = async (req, res) => {
     const lotname = req.params.lot_name
     const bayname = req.params.bay_name
     const bayList = await callDB('SELECT * FROM bays where parkinglot_name="' + lotname + '" AND bay_name="' + bayname + '"');
-
-    res.send(bayList[0])
+    if (bayList.length > 0)
+        res.send(bayList[0])
+    else
+        res.send({ "response": "Either bay name or lot name is wrong!" })
 };
 
 exports.getData = async (req, res) => {
     console.log(req.body)
-    // await callDB('')
+    const resp = await callDB('UPDATE SENSOR SET is_occupied =' + req.body.is_occupied + ' WHERE id="' + req.body.sensor_id + '"')
+    if (resp.affectedRows === 0)
+        res.send({ "response": "Sorry no such sensor is available!" })
+    else if (resp.changedRows === 0)
+        res.send({ "response": "Sensor value is already up to date!" })
+    else
+        res.send({ "response": "Successful!" })
 };
